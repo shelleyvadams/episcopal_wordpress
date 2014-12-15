5f025ed39ff214fcbebbbb7be2c10a90b7eeb3ac
shelleyvadams <shelleyvadams@gmail.com>
2014-12-15 12:16:59 -0500

    Replaced designone child themes with WordPress Theme Customizer setting for color scheme. Breaks header background and border in red and teal variants; will be fixed in the next commit.

---
+++ designone/js/theme-customizer.js
@@ -0,0 +1,24 @@
+(function( $ ) {
+	"use strict";
+
+	wp.customize( 'ed1_color_scheme', function( value ) {
+		value.bind( function( to ) {
+
+			var oldStylesheetURI = $( '#color-css' ).attr('href');
+			var newStylesheetURI;
+			var regEx = /\w+\.css/;
+			switch (to) {
+				case 'red':
+				case 'teal':
+					newStylesheetURI = oldStylesheetURI.replace(regEx, to + '.css');
+				break;
+				case 'blue':
+				default:
+					newStylesheetURI = oldStylesheetURI.replace(regEx, 'blue.css');
+				break;
+			}
+			$( '#color-css' ).attr('href', newStylesheetURI);
+		});
+	});
+
+})( jQuery );
