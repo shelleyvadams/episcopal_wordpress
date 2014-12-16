738ecd3fffa08b0b37693f530c8caa669a1318c3
shelleyvadams <shelleyvadams@gmail.com>
2014-12-16 15:55:32 -0500

    designone: remove functionality that belongs in plugins.

--- designone/metaboxes/map-meta.php
+++
@@ -1,8 +0,0 @@
-<div class="my_meta_control">
-
-	<label>Copy &amp; paste google maps embed code here</label>
-	<p>
-		<?php $mb->the_field('_map'); ?>
-		<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/>
-	</p>
-</div>
