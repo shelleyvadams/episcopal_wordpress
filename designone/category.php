6ebab6e7f8a02977718c91b23863f0bc51427602
shelleyvadams <shelleyvadams@gmail.com>
2014-12-16 21:15:20 -0500

    designone: add missing tags to category template so categories display properly.

--- designone/category.php
+++ designone/category.php
@@ -11,21 +11,29 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<?php if ( have_posts() ): ?>
-<h2>Category Archive: <?php echo single_cat_title( '', false ); ?></h2>
-<ol>
-<?php while ( have_posts() ) : the_post(); ?>
-	<li>
-		<article>
-			<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-			<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
-			<?php the_content(); ?>
-		</article>
-	</li>
-<?php endwhile; ?>
-</ol>
-<?php else: ?>
-<h2>No posts to display in <?php echo single_cat_title( '', false ); ?></h2>
-<?php endif; ?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/navigation' ) ); ?>
+
+<div class="section-wrapper clearfix">
+	<section>
+		<?php if ( have_posts() ): ?>
+		<h2>Category Archive: <?php echo single_cat_title( '', false ); ?></h2>
+		<ol>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<li>
+				<article>
+					<h2><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+					<time datetime="<?php the_time( 'Y-m-d' ); ?>" pubdate><?php the_date(); ?> <?php the_time(); ?></time> <?php comments_popup_link('Leave a Comment', '1 Comment', '% Comments'); ?>
+					<?php the_content(); ?>
+				</article>
+			</li>
+		<?php endwhile; ?>
+		</ol>
+		<?php else: ?>
+		<h2>No posts to display in <?php echo single_cat_title( '', false ); ?></h2>
+		<?php endif; ?>
+	</section>
+
+	<?php include_once 'sidebar.php'; ?>
+</div>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
