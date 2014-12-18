8c3c7e9a468cb856362844c722834dea0654a291
shelleyvadams <shelleyvadams@gmail.com>
2014-12-18 14:30:03 -0500

    designtwo: remove functionality better suited to a plugin.

--- designtwo/metaboxes/times-meta.php
+++
@@ -1,9 +0,0 @@
-<div class="my_meta_control">
-
-	<label>Times</label>
-
-	<p>
-		<?php $mb->the_field('_times'); ?>
-		<input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>"/>
-	</p>
-</div>
